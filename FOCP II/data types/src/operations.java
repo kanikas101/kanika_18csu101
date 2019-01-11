f47069a3fe688f0b5e4851d49f57ce8c66e56a20
kanikasehrawat <kanika18csu101@ncuindia.edu>
2019-01-11 11:20:50 +0530

    1

---
+++ FOCP II/data types/src/operations.java
@@ -0,0 +1,19 @@
+class operations
+{
+	public static void main(String args[])
+	{
+	int a,b,sum,diff,mul,div,mod;
+	a=10;
+	b=5;
+	sum=a+b;
+	diff=a-b;
+	mul=a*b;
+	div=a/b;
+	mod=a%b;
+	System.out.println("\nsum = " +sum);
+	System.out.println("\ndiff = " +diff);
+	System.out.println("\nmultiplication = " +mul);
+	System.out.println("\ndivision = " +div);
+	System.out.println("\nmodulus = " +mod);
+	}
+}
